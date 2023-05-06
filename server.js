62bfef9f8ba7d06e05c28e6dbfec63606be8c917
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-05-06 12:36:58 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/create', async (req, res) => {
+
+    const prompt = req.body.prompt;
+
+    const gptResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+    })
+
+    const image = gptResponse.data.data[0].url;
+    res.send({ image });
+});
+
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
